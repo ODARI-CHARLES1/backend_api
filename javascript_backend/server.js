b13a29bcc692d3f3f21c51aeeba508eb89777ce1
ODARI-CHARLES1 <daymondodari68@gmail.com>
2026-04-09 21:20:42 +0300

    Chore: add server.js file

---
+++ javascript_backend/server.js
@@ -0,0 +1,16 @@
+import cors from 'cors'
+import express from 'express'
+import {config} from 'dotenv'
+
+config()
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+const PORT=process.env.PORT || 3000
+
+app.get("/",(req,res)=>{
+    res.send("Server running..")
+})
+
+app.listen(PORT,()=>console.log("Server runnning..."))
