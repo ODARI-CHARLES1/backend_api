526de7008a66b534d9cdd1d6734ee5c5dd98264e
OdariKCharles1 <daymondodari68@gmail.com>
2026-04-23 07:24:47 +0300

    chore(javascript_backend): add mongodb configuration
    Add mongoose connection logic to the javascript_backend directory to facilitate database connectivity.

---
+++ javascript_backend/Config/mongodb.config.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+
+export default ConnectDB=async()=>{
+    const MONGO_URL=process.env.MONGO_URL
+    try {
+        await mongoose.connect(MONGO_URL)
+        console.log("DB connected successfully")
+    } catch (error) {
+        console.log("Failed to connect :",error)
+    }
+}
