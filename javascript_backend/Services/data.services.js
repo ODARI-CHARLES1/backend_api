d165d0c1588e37fbed3a7443e2ac5eee365a199d
OdariKCharles1 <daymondodari68@gmail.com>
2026-04-30 15:31:48 +0300

    feat(services): implement getData and getDataById services
    Add getDataService and getDataByIdService to handle data retrieval
    logic via the data repository, including error handling and
    conditional return statements.

--- javascript_backend/Services/data.services.js
+++ javascript_backend/Services/data.services.js
@@ -1,9 +1,37 @@
-import {saveData} from '../Repositories/data.repository.js'
+import * as dataRepository from '../Repositories/data.repository.js'
 
 export const saveDataService=async(temperature,humidity)=>{
     try {
-        await saveData(temperature,humidity)
+        await dataRepository.saveData(temperature,humidity)
     } catch (error) {
-        console.log("Failed at save data.")
+        console.error("Failed to service saveData",error)
+    }
+}
+
+export const getDataService=async()=>{
+   const data=await dataRepository.getData()
+   try {
+    if(data){
+        return data
+    }
+    else{
+        return ;
+    }
+   } catch (error) {
+    console.log("Failed to service getData",error)
+   }
+}
+
+export const getDataByIdService=async(id)=>{
+    const data=await dataRepository.getDataById(id)
+    try {
+        if(data){
+            return data
+        }
+        else{
+            return ;
+        }
+    } catch (error) {
+        console.error("Failed to service dataByid",error)
     }
 }
