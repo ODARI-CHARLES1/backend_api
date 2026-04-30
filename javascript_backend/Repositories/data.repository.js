e7e00bde699b945b2cdba5eabed12f9e089eea53
OdariKCharles1 <daymondodari68@gmail.com>
2026-04-30 15:29:43 +0300

    Chore: implementation of getdata nd getdatabyid repositories

--- javascript_backend/Repositories/data.repository.js
+++ javascript_backend/Repositories/data.repository.js
@@ -18,3 +18,34 @@ export const saveData = async (temperature, humidity) => {
   }
 };
 
+export const getData=async()=>{
+  try {
+    const data=await Data.find();
+    if(data){
+      return data
+      console.log("Data Retrieved successfully",data)
+    }
+    else{
+      return ;
+    }
+    
+  } catch (error) {
+    console.error("Error retrieving data",error)
+    throw error
+  }
+}
+
+export const getDataById=async(id)=>{
+  try {
+    const data=await Data.findById(`${id}`)
+    if(data){
+      return data
+    }
+    else{
+      return ;
+    }
+  } catch (error) {
+    console.error(`Error retrieving data with id ${id}`,error)
+    throw error
+  }
+}
